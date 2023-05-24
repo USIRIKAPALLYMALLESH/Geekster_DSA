89a116382d5d0feb02cbd6eb85d7965b9a786635
USIRIKAPLLY MALLESH <109076374+USIRIKAPALLYMALLESH@users.noreply.github.com>
2023-05-24 08:32:11 +0530

    Create Greater than 100 or not.java

---
+++ DSA/Module-1/Day-2/Greater than 100 or not.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int value=scn.nextInt();
+        if(value>100)
+            System.out.println(true);
+        else
+            System.out.println(false);
+    }
+}
