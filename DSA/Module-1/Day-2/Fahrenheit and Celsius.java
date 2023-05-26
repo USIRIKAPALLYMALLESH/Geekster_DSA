7e167bdf022dfc2720f239715b6fa9e7f1c1cf7f
USIRIKAPLLY MALLESH <109076374+USIRIKAPALLYMALLESH@users.noreply.github.com>
2023-05-26 16:21:42 +0530

    Create Fahrenheit and Celsius.java

---
+++ DSA/Module-1/Day-2/Fahrenheit and Celsius.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        double a=scn.nextDouble();
+        double ans=(a-32)*5/9;
+        System.out.println(ans);
+    }
+}
