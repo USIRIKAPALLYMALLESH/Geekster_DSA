0c520ea6af799ca137c9abe42c47aa62f4051216
USIRIKAPLLY MALLESH <109076374+USIRIKAPALLYMALLESH@users.noreply.github.com>
2023-05-26 16:17:56 +0530

    Create Sum and Difference of x and y.java

---
+++ DSA/Module-1/Day-2/Sum and Difference of x and y.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int a=scn.nextInt();
+        int b=scn.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+    }
+}
