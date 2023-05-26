9f49011ee4cdee4b203006f76faa6299ec715ded
USIRIKAPLLY MALLESH <109076374+USIRIKAPALLYMALLESH@users.noreply.github.com>
2023-05-26 16:26:47 +0530

    Create Area of triangle.java

---
+++ DSA/Module-1/Day-2/Area of triangle.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        double length=scn.nextDouble();
+        double breadth=scn.nextDouble();
+        double area=(length*breadth)/2;
+        System.out.println(area);
+    }
+}
