0b31358abd9a0541a8d1f861e5f66332da63a0a6
USIRIKAPLLY MALLESH <109076374+USIRIKAPALLYMALLESH@users.noreply.github.com>
2023-05-27 14:59:31 +0530

    Create Greater than 1000 or not.java

---
+++ DSA/Module-1/Day-3/Greater than 1000 or not.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int a =scn.nextInt();
+        if(a>1000)
+            System.out.println(true);
+        else
+            System.out.println(false);
+    }
+}
